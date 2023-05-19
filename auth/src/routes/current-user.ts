b0b4ab9e5c48aa1ae3932e1a2e1483b4dd03b032
Chaowei Liu <gba115566@gmail.com>
2023-05-19 21:50:49 +0800

    adding: return current-user

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+  // cookie session set?
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    // if setted
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    // if non-set
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
