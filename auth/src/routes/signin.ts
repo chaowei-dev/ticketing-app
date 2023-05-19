92fec80ed4b55e1ae531786f517fe2b401ec3584
Chaowei Liu <gba115566@gmail.com>
2023-05-19 19:57:22 +0800

    adding: sign in logic (pass-wrong and email-wrong)

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
 
+import { Password } from "../services/password";
+import { User } from "../models/user";
 import { validateRequest } from "../middlewares/validate-request";
+import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
 
@@ -12,7 +16,44 @@ router.post(
     body("password").trim().notEmpty().withMessage("You must suppy a password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    // user existing
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      // don't tell user email is wrong, just tell user something wrong
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // password wrong
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      // don't tell user password is wrong, just tell user something wrong
+      throw new BadRequestError("Invalid Credential");
+    }
+
+    // send JWT of existing user props
+    // #1 Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // #2 Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    // #3 send
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
