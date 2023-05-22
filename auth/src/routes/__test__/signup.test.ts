2d4271166dbc30c1e191e87a00517e1caee30567
Chaowei Liu <gba115566@gmail.com>
2023-05-22 19:49:34 +0800

    adding: testing invalid input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 
+// successful singnup
 it("returns a 201 on successful signup", async () => {
   return request(app)
     .post("/api/users/signup")
@@ -9,4 +10,58 @@ it("returns a 201 on successful signup", async () => {
       password: "passwrod",
     })
     .expect(201);
+});
+
+// invalid email
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "dsfasdfadsf",
+      password: "passwrod",
+    })
+    .expect(400);
+});
+
+// invalid password
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "dsfasdfadsf",
+      password: "p",
+    })
+    .expect(400);
+});
+
+// missing email and password
+it("returns a 400 with missing email and password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "dsfasdfadsf" })
+    .expect(400);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ password: "passwrod" })
+    .expect(400);
+});
+
+// disallows duplicate emails
+it("disallows duplicate emails", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
 });
