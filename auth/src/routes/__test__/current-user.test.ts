8e98528c335043633c930ab9061a862415329d7a
Chaowei Liu <gba115566@gmail.com>
2023-05-22 20:37:41 +0800

    adding: current-user funtionality and catch cookie

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import { app } from "../../app";
+
+// details with current user
+it("responds with details about the current user", async () => {
+  const authResponse = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "passwrod",
+    })
+    .expect(201);
+
+  // catch cookie
+  const cookie = authResponse.get("Set-Cookie");
+
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual("test@test.com");
+});
