5279905efa2bcdf52792956a9e7346cfb4262680
Chaowei Liu <gba115566@gmail.com>
2023-05-23 21:43:21 +0800

    adding: fetch data during SRR (on browser and on client)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,37 @@
+import axios from "axios";
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get("/api/users/currentuser").catch((err) => {
+  //   console.log(err.message);
+  // });
+
+  console.log(currentUser);
+
+  return <div>Landing Page</div>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // we are on the server!
+    // requests should be made to http://ingress-nginx.ingress-nginx
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    // we are on the brower
+    // requests can be made with a base url of ''
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
