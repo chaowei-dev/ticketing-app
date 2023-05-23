4fdc05b0a201af595d740c13618e8246b7a12a73
Chaowei Liu <gba115566@gmail.com>
2023-05-23 15:12:38 +0800

    adding: onSuccess callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
 import useRequest from "../../hooks/use-request";
 
 const signup = () => {
@@ -13,12 +13,15 @@ const signup = () => {
       email,
       password,
     },
+    onSuccess: () => {
+      Router.push("/");
+    },
   });
 
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    doRequest();
+    await doRequest();
   };
 
   return (
