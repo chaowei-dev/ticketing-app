063513601669878c471fdf16427143678989d980
Chaowei Liu <gba115566@gmail.com>
2023-05-25 23:50:02 +0800

    adding: test-update (non-own ticket)

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -16,10 +16,17 @@ router.put(
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
+    // no found
     if (!ticket) {
       throw new NotFoundError();
     }
 
+    // no auth
+    if (ticket.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    // found and auth
     res.send(ticket);
   }
 );
